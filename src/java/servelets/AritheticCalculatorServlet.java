6a1c71156f5b3636ea80f57df33dd82bd0264d55
[lynnneeesss] <[hyelin.park@edu.sait.ca]>
2023-03-07 18:22:37 -0700

    Week3 done

--- src/java/servelets/AritheticCalculatorServlet.java
+++ src/java/servelets/AritheticCalculatorServlet.java
@@ -20,6 +20,7 @@ public class AritheticCalculatorServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
         String firstInt = request.getParameter("fstint");
         String secondInt = request.getParameter("secint");
         
